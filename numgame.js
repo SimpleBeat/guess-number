7d8f0b46141e88f3e5fff45bafd41e0f93342d04
SimpleBeat <simplebeat@simplebeat.com>
2020-07-16 00:20:40 +0300

    add number entering and number reset functions

--- numgame.js
+++ numgame.js
@@ -1,3 +1,19 @@
+const digit10 = document.getElementById('d10')
+const digit1 = document.getElementById('d1')
+let enterPossible = true
+
 function buttonPressed(n) {
-    console.log(n)
+    if (enterPossible) {
+        digit10.innerText = digit1.innerText
+        digit1.innerText = n
+        if (digit10.innerText != 0) {
+            enterPossible = false
+        }
+    }
+}
+
+function resetPressed() {
+    digit1.innerText = 0
+    digit10.innerText = 0
+    enterPossible = true
 }
